cc25335477eb813b5948d771d164472cba9cadcb
GrishmaA <grish96@gmail.com>
2017-07-30 04:41:35 +0530

    final order - sakhi

--- sakhi_order.php
+++ sakhi_order.php
@@ -11,14 +11,18 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-$data=array();
-$q=mysqli_query($conn, "select flavor,quantity from product p JOIN sakhi_stock ss on p.id=ss.pid WHERE ss.sid='$sakhi_id'");
-while ($row=mysqli_fetch_object($q)){
+else
+{
+$quantity=$_POST['qty'];
+    $flavor=$_POST['flavor'];
+//$data=array();
+$q=mysqli_query($conn, "insert into sg_order(sid,pid,quantity) values('$sakhi_id',1,'10')select flavor,quantity from product p JOIN sakhi_stock ss on p.id=ss.pid WHERE ss.sid='$sakhi_id'");
+/*while ($row=mysqli_fetch_object($q)){
     $data[]=$row;
-}
+}*/
 
 echo json_encode($data);
+}
 ?>
 
 <!--
