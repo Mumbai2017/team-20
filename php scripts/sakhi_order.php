bb94ca99011fb670cbb37fb82a91f499657233fa
GrishmaA <grish96@gmail.com>
2017-07-30 01:47:42 +0530

    sakhi stiock and php

--- php scripts/sakhi_order.php
+++ php scripts/sakhi_order.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "";
 $dbname = "sanisa";
 
 // Create connection
@@ -13,8 +13,9 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$q=mysqli_query($con,"insert into sg_order values(3,1,"10")");
-if(mysqli_query($con,$q))
+$q=mysqli_query($conn,"insert into sg_order(sid,pid,quantity) values(1,1,'10')");
+
+if($q)
 		 {
 			$resultOfQuery = 1;
 		 }
