e3c1ef6cd81fcef14edfd09bb3b2b757b82a5852
GrishmaA <grish96@gmail.com>
2017-07-30 03:23:52 +0530

    sakhi order

--- php scripts/sakhi_order.php
+++ php scripts/sakhi_order.php
@@ -5,6 +5,23 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "sanisa";
+$sakhi_id = 2;
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$data=array();
+$q=mysqli_query($conn, "select flavor,quantity from product p JOIN sakhi_stock ss on p.id=ss.pid WHERE ss.sid='$sakhi_id'");
+while ($row=mysqli_fetch_object($q)){
+    $data[]=$row;
+}
+
+echo json_encode($data);
+?>
+
+<!--
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -16,7 +33,7 @@ if (!$conn) {
 $q=mysqli_query($conn,"insert into sg_order(sid,pid,quantity) values(1,1,'10')");
 
 if($q)
-		 {
+		 //{
 			$resultOfQuery = 1;
 		 }
 		 else
@@ -26,4 +43,4 @@ if($q)
 		 return $resultOfQuery;
 
 echo json_encode($resultOfQuery);
-?>
+?>-->
