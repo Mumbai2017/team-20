f1e970912b65015061062b5324298add365f95f8
VAIBHAVHATISKAR21 <vaibhav.hatiskar21@gmail.com>
2017-07-30 06:19:26 +0530

    Merge branch 'master' of https://github.com/Mumbai2017/team-20

---
+++ gu/php/data.php
@@ -0,0 +1,48 @@
+<?php
+
+require("phpsqlajax.php");
+
+// Start XML file, create parent node
+
+$dom = new DOMDocument("1.0");
+$node = $dom->createElement("markers");
+$parnode = $dom->appendChild($node);
+
+// Opens a connection to a MySQL server
+
+$connection=mysqli_connect( "localhost", "root", "root", $database ) or die("Could not connect: " .mysqli_error($conn) );
+if (!$connection) {  die('Not connected : ' . mysql_error());}
+
+// Set the active MySQL database
+
+
+// Select all the rows in the markers table
+
+$query = "Select p.flavor, round((sum(sg.quantity)*100)/(Select sum(quantity) from sg_order),2)  from product p, sg_order sg
+where p.id=sg.pid
+group by p.flavor";
+$result = mysqli_query($connection,$query);
+if (!$result) {
+  die('Invalid query: ' . mysql_error());
+}
+
+header("Content-type: text/xml");
+
+// Iterate through the rows, adding XML nodes for each
+
+while ($row = @mysqli_fetch_assoc($result))
+        {
+  // Add to XML document node
+  $node = $dom->createElement("marker");
+  $newnode = $parnode->appendChild($node);
+  $newnode->setAttribute("id",$row['id']);
+  $newnode->setAttribute("name",$row['name']);
+  $newnode->setAttribute("address", $row['address']);
+  $newnode->setAttribute("lat", $row['lat']);
+  $newnode->setAttribute("lng", $row['lng']);
+  $newnode->setAttribute("type", $row['type']);
+}
+
+echo $dom->saveXML();
+
+?>
---
+++ sakhi_orders1.php
@@ -0,0 +1,23 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sanisa";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$data=array();
+$q=mysqli_query($conn, "select name,address,contact_no,flavor,quantity from customer c JOIN c_order co on c.id=co.cid JOIN product p on co.pid=p.id ");
+while ($row=mysqli_fetch_object($q)){
+    $data[]=$row;
+}
+
+echo json_encode($data);
+?>
